9ab230149e30c45b0ab25f52c38020b7a420cb1f
shreyasrama <shreyas.rama1@gmail.com>
2024-09-09 21:04:03 +1200

    Add svelte-gestures and initial support for swiping on entry screen

--- src/lib/components/entry/entry.svelte
+++ src/lib/components/entry/entry.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import { getEntriesForToday, insertNewEntry } from "$lib/db/db-functions";
+    import { Textarea } from "$lib/components/ui/textarea";
     
+    import { swipe, type SwipeCustomEvent } from 'svelte-gestures';
     import { toast } from "svelte-sonner";
     import LoaderCircle from "lucide-svelte/icons/loader-circle";
 
 	import { fade } from "svelte/transition";
-	import { Textarea } from "$lib/components/ui/textarea";
 
     const date = new Date();
     const day = date.toLocaleDateString('en-US', {weekday: 'long'});
@@ -14,6 +15,8 @@
 
     let detailList: {detail: string}[] = [];
     let detailInput: string = '';
+
+    let direction;
 
     function handleEnter(event: KeyboardEvent) {
         if (event.key === 'Enter' && /\d|[A-z]/.test(detailInput)) {
@@ -28,60 +31,66 @@
         }
     }
 
+    function handleSwipe(event: SwipeCustomEvent) {
+        //
+    }
+
     document.querySelector('textarea')?.addEventListener("input", function(){
         this.style.height = '0px';
         this.style.height = this.scrollHeight + 'px';
     })
 </script>
 
-<div class="flex flex-col items-center">
-    <h2 class="scroll-m-20 text-3xl font-medium opacity-70 tracking-tight transition-colors mt-12 first:mt-0 ">
-        Today is
-    </h2>
-    
-    <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight py-4 lg:text-5xl">
-        {day}, {month} {dayNum}
-    </h1>
-    
-    <h2 class="scroll-m-20 text-3xl font-medium opacity-70 tracking-tight transition-colors first:mt-0">
-        What did you get done today?
-    </h2>
-
-    <Textarea
-        bind:value={detailInput} 
-        on:keyup={handleEnter}
-        placeholder="Press Enter to apply..."
-        class="resize-none border-none text-center min-h-40 max-w-xs mt-8 placeholder:italic focus:!ring-transparent"
-        autofocus
-      />
+<div class="w-full h-full" use:swipe={{ timeframe: 300, minSwipeDistance: 100 }} on:swipe={handleSwipe}>
+    <div class="flex flex-col items-center">
+        <h2 class="scroll-m-20 text-3xl font-medium opacity-70 tracking-tight transition-colors mt-12 first:mt-0 ">
+            Today is
+        </h2>
         
+        <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight py-4 lg:text-5xl">
+            {day}, {month} {dayNum}
+        </h1>
         
-    {#await getEntriesForToday()}
-        <LoaderCircle class="block mx-auto my-4 h-6 w-6 animate-spin" />
-    {:then entries}
-        {#if entries.length > 0 || detailList.length > 0}
-            <h2 class="scroll-m-20 text-3xl font-medium opacity-70 tracking-tight transition-colors mt-12 first:mt-0 ">
-                Earlier today
-            </h2>
-
-            <ul class="list-none my-6 text-center [&>li]:mt-2">
-                {#each entries as entry}
-                    <li
+        <h2 class="scroll-m-20 text-3xl font-medium opacity-70 tracking-tight transition-colors first:mt-0">
+            What did you get done today?
+        </h2>
+    
+        <Textarea
+            bind:value={detailInput} 
+            on:keyup={handleEnter}
+            placeholder="Press Enter to apply..."
+            class="resize-none border-none text-center min-h-40 max-w-xs mt-8 placeholder:italic focus:!ring-transparent"
+            autofocus
+          />
+            
+            
+        {#await getEntriesForToday()}
+            <LoaderCircle class="block mx-auto my-4 h-6 w-6 animate-spin" />
+        {:then entries}
+            {#if entries.length > 0 || detailList.length > 0}
+                <h2 class="scroll-m-20 text-3xl font-medium opacity-70 tracking-tight transition-colors mt-12 first:mt-0 ">
+                    Earlier today
+                </h2>
+    
+                <ul class="list-none my-6 text-center [&>li]:mt-2">
+                    {#each entries as entry}
+                        <li
+                            transition:fade={{ delay: 250, duration: 300 }}
+                            class="mb-4"
+                        >
+                            {entry.detail}
+                        </li>                 
+                    {/each}
+                    {#each  detailList as detail}
+                        <li
                         transition:fade={{ delay: 250, duration: 300 }}
                         class="mb-4"
                     >
-                        {entry.detail}
-                    </li>                 
-                {/each}
-                {#each  detailList as detail}
-                    <li
-                    transition:fade={{ delay: 250, duration: 300 }}
-                    class="mb-4"
-                >
-                    {detail.detail} 
-                    </li>
-                {/each}
-            </ul>
-        {/if}
-    {/await}
+                        {detail.detail} 
+                        </li>
+                    {/each}
+                </ul>
+            {/if}
+        {/await}
+    </div>
 </div>
