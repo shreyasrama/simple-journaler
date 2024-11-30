ee16c1f18166986405a00caf334309197e40a5c5
shreyasrama <shreyas.rama1@gmail.com>
2024-11-30 19:49:13 +1300

    Try remove CF middleware

--- functions/_middleware.js
+++
@@ -1,7 +0,0 @@
-export const onRequest = async (context) => {
-    const response = await context.next();
-    response.headers.set('Cross-Origin-Embedder-Policy', 'require-corp');
-    response.headers.set('Cross-Origin-Opener-Policy', 'same-origin');
-
-    return response;
-};
--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,7 @@ export default defineConfig({
     optimizeDeps: {
         exclude: ['sqlocal']
     },
+    server: {
+        port: 5175
+    }
 });
