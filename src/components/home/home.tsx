5b3259bac46dcb767669380bda37453d3c9171be
shreyasrama <shreyas.rama1@gmail.com>
2025-11-22 08:20:34 +1300

    refactor Home to use ClientOnly instead of useEFfect

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -1,33 +1,32 @@
-import { useEffect, useState } from "react";
-import { useHydrated } from "@tanstack/react-router";
+import { useState } from "react";
+import { ClientOnly } from "@tanstack/react-router";
 import { SQLocalDrizzle } from "sqlocal/drizzle";
 import { drizzle } from "drizzle-orm/sqlite-proxy";
 
 import { isInitialized } from "@/db/init";
 
-export default function Home() {
-  const hydrated = useHydrated();
+function HomeMessage() {
   const [isNewUser, setIsNewUser] = useState<boolean>(true);
 
-  useEffect(() => {
-    const initDB = async () => {
-      if (hydrated) {
-        const { driver, batchDriver } = new SQLocalDrizzle(
-          "simplejournaler.sqlite3",
-        );
-        const db = drizzle(driver, batchDriver);
+  const initCheck = async () => {
+    const { driver, batchDriver } = new SQLocalDrizzle(
+      "simplejournaler.sqlite3",
+    );
+    const db = drizzle(driver, batchDriver);
 
-        const existingUser = await isInitialized(db);
-        setIsNewUser(existingUser ? false : true);
-      }
-    };
+    const existingUser = await isInitialized(db);
+    setIsNewUser(existingUser ? false : true);
+  };
 
-    initDB();
-  }, [hydrated]);
-
-  if (!hydrated) {
-    return <div>Loading...</div>;
-  }
+  initCheck();
 
   return <div>{isNewUser ? "New user" : "Existing user"}</div>;
+}
+
+export default function Home() {
+  return (
+    <ClientOnly fallback={null}>
+      <HomeMessage />
+    </ClientOnly>
+  );
 }
