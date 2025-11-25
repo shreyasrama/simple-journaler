feab91b0d7bcf790f1d57c3b63d0bcb385867beb
shreyasrama <shreyas.rama1@gmail.com>
2025-11-25 14:45:27 +1300

    add lexical editor to entry

--- src/components/entry/entry.tsx
+++ src/components/entry/entry.tsx
@@ -1,13 +1,17 @@
 import { ClientOnly } from "@tanstack/react-router";
 
 import TabBar from "@/components/layout/tab-bar";
+import Editor from "./lexical";
 
 export default function Entry() {
   return (
     <ClientOnly fallback={null}>
       <TabBar />
 
-      <div>Hello "/entry"!</div>
+      <h1>Welcome, user. Today is date.</h1>
+      <h2>What did you get up to today?</h2>
+
+      <Editor />
     </ClientOnly>
   );
 }
